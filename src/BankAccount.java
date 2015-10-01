3d467e651db5242805375334d4dabc71d58146b0
ahdieh <aemadi@my.centennialcollege.ca>
2015-09-30 23:07:01 -0700

    Final edit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,54 +1,70 @@
 
 public class BankAccount {
+	
+	// PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++
 	private String _accountNumber;
 	private String _ownersName;
 	private double _balance;
 	
 	
 	// PROPERTIES +++++++++++++++++++++++++++++++++++++++++++++++
-			public String getAccountNumber() {
-				return this._accountNumber;
-			}
+	public String getAccountNumber() {
+		return this._accountNumber;
+	}
 
-			public void setAccountNumber(String accountNumber) {
-				this._accountNumber = accountNumber;
-			}
+	public void setAccountNumber(String accountNumber) {
+		this._accountNumber = accountNumber;
+	}
 			
-			public String getOwnersName() {
-				return this._ownersName;
-			}
+	public String getOwnersName() {
+		return this._ownersName;
+	}
 			
-			public void setOwnersName(String ownersName) {
-				this._ownersName = ownersName;
-			}
+	public void setOwnersName(String ownersName) {
+		this._ownersName = ownersName;
+	}
 			
-			public double getBalance() {
-				return this._balance;
-			}
-			public void setBalance(double balance) {
-				this._balance = balance;
-			}
+	public double getBalance() {
+		return this._balance;
+	}
+	
+	public void setBalance(double balance) {
+		this._balance = balance;
+	}
 			
-			@SuppressWarnings("unused")
-			private void _initialize(String accountNumber, String ownersName , double balance) {
-				this.setAccountNumber(accountNumber);
-				this.setOwnersName(ownersName);
-				this.setBalance(balance);
-			}
-			public void Deposit(double myDeposit){
-				this._balance = this._balance + myDeposit;
-				System.out.printf("Your balance after deposting is: $%.2f",this._balance);
-				System.out.println();
-			}
+	// CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++
+     public BankAccount(){
+			this._initialize("Unknowm", "Unknowm",0.0);
+
+    }
+     
+	// PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++
+
+	 private void _initialize(String accountNumber, String ownersName , double balance) {
+			this.setAccountNumber(accountNumber);
+			this.setOwnersName(ownersName);
+			this.setBalance(balance);
+	 }
+	 
+	// PUBLIC METHODS +++++++++++++++++++++++++++++++++++++++++
+	public void Deposit(double myDeposit){
+			this._balance = this._balance + myDeposit;
+			System.out.printf("Your balance after deposting is: $%.2f \n",this.getBalance());
+			System.out.println();
+
+    }
 			
-			public void Withdraw(double myWithdraw){
-				this._balance = this._balance - myWithdraw;
-				System.out.printf("Your balance after withdrawing is: $%.2f",this._balance);
-				System.out.println();
-			}
+	public void Withdraw(double myWithdraw){
+	 		this._balance = this._balance - myWithdraw;
+			System.out.printf("Your balance after withdrawing is: $%.2f",this.getBalance());
+			System.out.println();
+
+	}
 			
-			public  void getAccountInfo () {
-				System.out.printf("***Account Number: %s%n***Name: %s%n***Balance: $%.2f%n",
-						this._accountNumber, this._ownersName, this._balance);
-			}
+	public  void getAccountInfo () {
+		    System.out.println();
+	    	System.out.printf("***%s's Account Information:	\n",this.getOwnersName());
+	    	System.out.println();
+
+	}
 }
---
+++ src/BankAccountTest.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+
+public class BankAccountTest  {
+	public static BankAccount bankAccount; // reference to the Person class
+	public static void main(String[] args) {
+		System.out.println("Program Started...");
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt user for Account Number
+		System.out.print("Enter your Account Number: ");
+		String myAccountNumber= input.nextLine();
+		
+		// Prompt user for Owners'name
+		System.out.print("Enter your Name: ");
+		String myOwnersName= input.nextLine();
+		
+		// Prompt user for Balance
+		System.out.print("Enter your Initial Balance: ");
+		Double myBalance= input.nextDouble();
+		
+				
+		
+		bankAccount = new BankAccount();
+		
+		bankAccount.setAccountNumber(myAccountNumber);
+		bankAccount.setOwnersName(myOwnersName);
+		bankAccount.setBalance(myBalance);
+		
+		// Prompt user for Deposit
+		System.out.print("Enter the ammount of Deposit: ");
+		Double myDeposit = input.nextDouble();
+		bankAccount.Deposit(myDeposit);
+		
+		// Prompt user for Withdraw
+		System.out.print("Enter the ammount of Withdraw: ");
+		Double myWithdraw = input.nextDouble();
+		bankAccount.Withdraw(myWithdraw);
+	
+			
+		bankAccount.getAccountInfo();
+		
+		System.out.printf("After transaction %s with the Account#%s has $%.2f.",bankAccount.getOwnersName(),bankAccount.getAccountNumber(),bankAccount.getBalance());
+
+		
+		
+	}
+
+}
